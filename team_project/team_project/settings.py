bfd20c62fa7980260ad9885bdb02148cbff581dc
Xinyi Li <xinyil8023@gmail.com>
2025-03-31 11:49:23 -0700

    configuration

--- team_project/team_project/settings.py
+++ team_project/team_project/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'accounts',
     'dogs',
     'expenses',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
--- team_project/team_project/urls.py
+++ team_project/team_project/urls.py
@@ -19,6 +19,8 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('accounts.urls') )
+    path('', include('accounts.urls') ),
+    path('', include('dogs.urls') ),
+    path('', include('expenses.urls') ),
 ]
 
