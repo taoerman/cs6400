81b8e787e240a08de9cd3b5969ff4b7e760f75d7
taoerman <qiliu0523@gmail.com>
2025-03-31 11:20:32 -0700

    add code

---
+++ team_project/accounts/management/commands/create_Adoption.py
@@ -0,0 +1,20 @@
+from django.db import connection
+from django.core.management.base import BaseCommand
+
+class Command(BaseCommand):
+    help = "Create Dog table"
+
+    def handle(self, *args, **kwargs):
+        with connection.cursor() as cursor:
+            cursor.execute("""
+                            CREATE TABLE IF NOT EXISTS Adoption (
+                                adoptionID INT AUTO_INCREMENT PRIMARY KEY,
+                                dogID INT NOT NULL,
+                                adopterID INT NOT NULL,
+                                applicationStatus VARCHAR(20) NOT NULL CHECK (applicationStatus IN ('pending approval', 'approved', 'rejected')),
+                                startDate DATE NOT NULL,
+                                FOREIGN KEY (dogID) REFERENCES Dog(id),
+                                FOREIGN KEY (adopterID) REFERENCES Adopter(adopterID)
+                            );
+                        """)
+        self.stdout.write(self.style.SUCCESS("adoption table created."))
