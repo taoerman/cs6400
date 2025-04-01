4840d0916e1dff434127b3b130300481549499b1
Jialiang Huang <jhuang806@gatech.com>
2025-03-31 23:38:02 -0700

    fixed dog dashboard

--- front_end/app/Dog/Dogdashboard.js
+++ front_end/app/Dog/Dogdashboard.js
@@ -3,7 +3,7 @@ export const Dogdashboard = () => {
   const [data, setData] = useState([]);
   useEffect(() => {
     async function loadData() {
-      const res = await fetch('http://localhost:8000/dogs'); 
+      const res = await fetch('http://localhost:8080/dogs/get_all_dogs/'); 
       const result = await res.json();
       setData(result);
     }
@@ -30,18 +30,18 @@ export const Dogdashboard = () => {
         </thead>
         <tbody className="text-gray-800">
           {data.map((dog) => (
-            <tr key={dog.dogID} className="hover:bg-gray-50">
-              <td className="px-4 py-2 border">{dog.dogID}</td>
+            <tr key={dog.dog_ID} className="hover:bg-gray-50">
+              <td className="px-4 py-2 border">{dog.dog_ID}</td>
               <td className="px-4 py-2 border">{dog.name}</td>
-              <td className="px-4 py-2 border">{JSON.parse(dog.breed).join(', ')}</td>
+              <td className="px-4 py-2 border">{dog.breed? JSON.parse(dog.breed).join(', ') : ''}</td>
               <td className="px-4 py-2 border">{dog.sex}</td>
               <td className="px-4 py-2 border">{dog.altered ? 'Yes' : 'No'}</td>
-              <td className="px-4 py-2 border">{dog.ageForMonths}</td>
+              <td className="px-4 py-2 border">{dog.age_for_months}</td>
               <td className="px-4 py-2 border">{dog.description}</td>
-              <td className="px-4 py-2 border">{dog.microchipID}</td>
+              <td className="px-4 py-2 border">{dog.microchip_ID}</td>
               <td className="px-4 py-2 border">{dog.microchipVendor}</td>
-              <td className="px-4 py-2 border">{dog.surrenderDate}</td>
-              <td className="px-4 py-2 border">{dog.surrenderPhone}</td>
+              <td className="px-4 py-2 border">{dog.surrender_date}</td>
+              <td className="px-4 py-2 border">{dog.surrender_phone}</td>
               <td className="px-4 py-2 border">{dog.surrenderedByAnimalControl ? 'Yes' : 'No'}</td>
             </tr>
           ))}
