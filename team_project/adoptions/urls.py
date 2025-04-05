c4d397f787146271a866890545405d043ff1c6e8
taoerman <qiliu0523@gmail.com>
2025-04-05 10:36:15 -0700

    is_adopted

--- team_project/adoptions/urls.py
+++ team_project/adoptions/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('adoptions/review_pending_applications/', views.review_pending_applications),
     path('adoptions/finalize_adoption/', views.finalize_adoption),
     path('adoptions/get_all_adoptions/', views.get_all_adoptions),
+    path('adoptions/is_adopted/', views.is_adopted),
 ]
