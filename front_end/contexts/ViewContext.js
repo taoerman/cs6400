eaf1c672a34c79d081b2913aa3f40649d9a89f0d
Xinyi Li <xinyil8023@gmail.com>
2025-04-05 21:34:52 -0700

    Monthly report.

--- front_end/contexts/ViewContext.js
+++ front_end/contexts/ViewContext.js
@@ -6,10 +6,11 @@ const ViewContext = createContext();
 
 export const ViewProvider = ({ children }) => {
   const [currentView, setCurrentView] = useState(1);
+  const [currentReport, setCurrentReport] = useState('adoptionView');
   const [dogId, setDogId] = useState(1);
   const [userType, setUserType] = useState(1)
   return (
-    <ViewContext.Provider value={{ currentView, setCurrentView, dogId, setDogId, userType, setUserType }}>
+    <ViewContext.Provider value={{ currentView, setCurrentView, currentReport, setCurrentReport, dogId, setDogId, userType, setUserType }}>
       {children}
     </ViewContext.Provider>
   );
